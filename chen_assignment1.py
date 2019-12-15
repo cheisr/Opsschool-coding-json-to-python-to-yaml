1d4e812ed380d2cec9e4642bb18238e2bef60e74
Chen <chenisrael@chens-mbp.mynet>
2019-12-15 20:22:17 +0200

    modified file

--- chen_assignment1.py
+++ chen_assignment1.py
@@ -1,36 +1,23 @@
-if __name__ == '__main__':
-
-    import json
-    import operator
-    import yaml
-
+import json
+import operator
+import yaml
 
 with open('hw.json') as myfile:
-        data = myfile.read()
-
+    data = myfile.read()
 parsed_json = json.loads(data)
-print(parsed_json)
 
 ppl_ages_dict = parsed_json["ppl_ages"]
 age_buckets_list = parsed_json["buckets"]
 
-print("ages dict: ", ppl_ages_dict)
-print("buckets list: ", age_buckets_list)
-
 max_age = max(ppl_ages_dict.items(), key=operator.itemgetter(1))[1]
 max_age += 1
-print("max age: ", max_age)
 
 age_buckets_list += [0, max_age]
 age_buckets_list.sort()
-print("buckets list: ", age_buckets_list)
-
 buckets_counter = len(age_buckets_list)
-print(age_buckets_list, buckets_counter)
 
 keystring = "{0} - {1}"
 result = {}
-
 for i in range(1, buckets_counter):
     values_list = []
     dictkey = keystring.format(age_buckets_list[i-1], age_buckets_list[i])
